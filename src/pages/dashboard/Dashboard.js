f04bd63c2c9c4e3bea08bebe239ff1d3c0d9c0a0
Jairo Lozano <jairo25492@gmail.com>
2022-09-25 15:27:26 -0600

    Add new project

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,18 @@
+import {useCollection} from "../../hooks/useCollection";
+
 // styles
 import './Dashboard.css'
+import ProjectList from "../../components/ProjectList";
 
-export default function Dashboard(){
+export default function Dashboard() {
+
+    const {documents, error} = useCollection('projects');
+
     return (
         <div>
-            Dashboard
+            <h2 className={"page-title"}>Dashboard</h2>
+            {error && <p className={"error"}>{error}</p>}
+            {documents && <ProjectList projects={documents}/>}
         </div>
     )
 }
